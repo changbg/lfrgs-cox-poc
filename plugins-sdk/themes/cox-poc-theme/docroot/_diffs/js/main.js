7ae2f7ed16b23a4f82e49a192da02d07669bfd37
Travis Cory <tcory@tcory-2.local>
2015-12-02 12:08:09 -0800

    Adding third level navigation to theme

--- plugins-sdk/themes/cox-poc-theme/docroot/_diffs/js/main.js
+++ plugins-sdk/themes/cox-poc-theme/docroot/_diffs/js/main.js
@@ -1,6 +1,38 @@
 AUI().use(
 	'liferay-sign-in-modal',
 	function(A) {
+		var wrapper = A.one('#wrapper');
+
+		if (wrapper) {
+			wrapper.delegate(
+				'click',
+				function(event) {
+					var ct = event.currentTarget;
+					var dropdown = ct.ancestor('.dropdown');
+
+					event.preventDefault();
+
+					if (dropdown) {
+						if (dropdown.hasClass('open')) {
+							dropdown.removeClass('open');
+						}
+						else {
+							dropdown.addClass('open');
+						}
+
+						ct.on(
+							'clickoutside',
+							function() {
+								dropdown.removeClass('open');
+								this.detach();
+							}
+						);
+					}
+				},
+				'.dropdown-toggle'
+			);
+		}
+
 		if (!Liferay.ThemeDisplay.isSignedIn()) {
 			var signIn = A.one('.btn-sign-in');
 
